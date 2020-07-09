5d7b6b1934e7cf90c574873542739afac510a891
shahil04 <ansarirahish455@gmail.com>
2020-07-09 15:21:59 +0530

    initial commit

---
+++ Dice_rolling_game.py
@@ -0,0 +1,11 @@
+import random
+
+print('Dice Roll Game!')
+dice=randon.randint(1,6)
+
+if dice==1:
+	print('@-----@')
+	print('|     |')
+	print('|  *  |')
+	print('|     |')
+	print('@-----@')
---
+++ guess-number-game.py
@@ -0,0 +1,44 @@
+import random
+
+def guess_game():
+	random_number=random.randint(0,10)
+	chance=3
+
+	print('#######_______guessing Random Number Game_______#########')
+	print('\n')
+	print('____________start game!__________')
+	print('\n')
+
+	while(chance>0):
+		print('you have total 3 chance ')
+		num=input("enter the your guess number ")
+		print('\n')
+
+		if num==random_number:
+			print('your guess number and random number is same that is '+str(num))
+			print("yes you guess the number ")
+			print("________great job________")
+			print('\n')
+			print('\n')
+
+		elif random_number+1==num or random_number-1==num:
+			print('your guess is very near! ')
+			print('it is '+str(random_number))
+			print('your number is '+str(num))
+			chance=chance-1
+			print('left chance= '+str(chance))
+			print('\n')
+			print('\n')
+
+
+		else:
+			print("you guess wrong try again!")
+			print('random number is '+str(random_number))
+			chance=chance-1
+			print('left chance= '+str(chance))
+			print('\n')
+			print('\n')
+
+	print('------------game over!--------------')
+	print('\n')	
+guess_game()
